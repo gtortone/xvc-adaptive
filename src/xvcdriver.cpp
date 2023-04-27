0b583e2bf27f0ba299fabed2624ee78f36c3ace4
Gennaro Tortone <gennaro.tortone@na.infn.it>
2023-04-27 11:14:06 +0200

    introducing std::vector for JTAG buffers

--- src/xvcdriver.cpp
+++ src/xvcdriver.cpp
@@ -1,4 +1,5 @@
 #include "xvcdriver.h"
+#include <vector>
 
 XVCDriver::XVCDriver(void) {
 }
@@ -12,28 +13,25 @@ uint32_t XVCDriver::probeIdCode(void) {
 
    printDebug("XVCDriver::probeIdCode start", 1);
 
-   unsigned char buffer[8];
+   std::vector<unsigned char> buffer;
    unsigned char result[4];
    uint32_t *idcode;
    int nbits;
 
    // TEST-LOGIC-RESET
-   buffer[0] = 0xFF;
-   buffer[1] = 0x00;
+   buffer = {0xFF, 0x00};
    nbits = 5;
-   shift(nbits, buffer, result);
+   shift(nbits, buffer.data(), result);
 
    // RUN-TEST/IDLE
-   buffer[0] = 0x00;
-   buffer[1] = 0x00;
+   buffer = {0x00, 0x00};
    nbits = 1;
-   shift(nbits, buffer, result);
+   shift(nbits, buffer.data(), result);
 
    // SHIFT-IR
-   buffer[0] = 0x03;
-   buffer[1] = 0x00;
+   buffer = {0x03, 0x00};
    nbits = 4;
-   shift(nbits, buffer, result);
+   shift(nbits, buffer.data(), result);
 
    // set IDcode instruction and navigate to EXIT-IR on last bit
    uint32_t tms = 1 << (irlen-1);
@@ -44,30 +42,21 @@ uint32_t XVCDriver::probeIdCode(void) {
       buffer[i+nbytes] = (tdi >> 8*i) & 0x000000FF;
    }
    nbits = irlen;
-   shift(nbits, buffer, result);
+   shift(nbits, buffer.data(), result);
 
    // UPDATE-IR
-   buffer[0] = 0x01;
-   buffer[1] = 0x00;
+   buffer = {0x01, 0x00};
    nbits = 2;
-   shift(nbits, buffer, result);
+   shift(nbits, buffer.data(), result);
 
    // SHIFT-DR
-   buffer[0] = 0x01;
-   buffer[1] = 0x00;
+   buffer = {0x01, 0x00};
    nbits = 3;
-   shift(nbits, buffer, result);
+   shift(nbits, buffer.data(), result);
 
-   buffer[0] = 0x00;
-   buffer[1] = 0x00;
-   buffer[2] = 0x00;
-   buffer[3] = 0x80;
-   buffer[4] = 0x00;
-   buffer[5] = 0x00;
-   buffer[6] = 0x00;
-   buffer[7] = 0x00;
+   buffer = {0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00, 0x00};
    nbits = 32;
-   shift(nbits, buffer, result);
+   shift(nbits, buffer.data(), result);
 
    idcode = reinterpret_cast<uint32_t *>(result);
    
@@ -80,30 +69,20 @@ uint32_t XVCDriver::scanChain(void) {
 
    printDebug("XVCDriver::scanChain start", 3);
 
-   unsigned char buffer[12];
+   std::vector<unsigned char> buffer;
    unsigned char result[6];
    uint32_t idcode32;
    uint64_t *idcode64;
    int nbits;
 
    // TMS
-   buffer[0] = 0x5F;       // 0000.0000.0000.0000.0000.0000.0000.0000.0000.0000.0101.1111
-   buffer[1] = 0x00;
-   buffer[2] = 0x00;
-   buffer[3] = 0x00;
-   buffer[4] = 0x00;
-   buffer[5] = 0x00;
-
+   // 0000.0000.0000.0000.0000.0000.0000.0000.0000.0000.0101.1111
    // TDI
-   buffer[6] = 0x00;       // 0000.0001.1111.1111.1111.1111.1111.1111.1111.1110.0000.0000
-   buffer[7] = 0xFE;
-   buffer[8] = 0xFF;
-   buffer[9] = 0xFF;
-   buffer[10] = 0xFF;
-   buffer[11] = 0x01;         
-   
+   // 0000.0001.1111.1111.1111.1111.1111.1111.1111.1110.0000.0000
+
+   buffer = {0x5F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFE, 0xFF, 0xFF, 0xFF, 0x01};
    nbits = 41;
-   shift(nbits, buffer, result);
+   shift(nbits, buffer.data(), result);
 
    idcode64 = reinterpret_cast<uint64_t *>(result);
    idcode32 = (uint32_t) ( (*idcode64 >> 9) & (0xFFFFFFFF) );
